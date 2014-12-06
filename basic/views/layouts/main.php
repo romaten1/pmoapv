d354e3b9bcf4938a11240364a5ac188527417113
romaten1 <romaten1@rambler.ru>
2014-12-06 22:51:53 +0200

    Main menu

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -26,21 +26,61 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'ПМОАПВ',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
+                    'class' => 'navbar navbar-fixed-top',
                 ],
             ]);
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
+                    ['label' => 'Про кафедру',
+                        'items' => [
+                            ['label' => 'Про кафедру', 'url' => ['/kafedra/about']],
+                            ['label' => 'Історія кафедри', 'url' => ['/kafedra/history']],
+                            ['label' => 'Викладацький склад', 'url' => ['/kafedra/teachers']],
+                            ['label' => 'Методична робота', 'url' => ['/kafedra/metod_work']],
+                            ['label' => 'Організацйна робота', 'url' => ['/kafedra/org_work']],
+                            ['label' => 'Практична підготовка студентів', 'url' => ['/kafedra/praktika']],
+                            ['label' => 'Навчальні лабораторії', 'url' => ['/kafedra/laboratory']],
+                            ['label' => 'Матеріально-технічна база', 'url' => ['/kafedra/mattehbaza']],
+                            ['label' => 'Контакти', 'url' => ['/kafedra/contact']],
+                        ],
+                    ],
+                    ['label' => 'Новини', 'url' => ['/site/about']],
+                    ['label' => 'Абітурієнту',
+                        'items' => [
+                            ['label' => 'Звернення до наших абітурієнтів', 'url' => ['/abiturient/about']],
+                            ['label' => 'Напрям підготовки 6.100202 Процеси, машини та обладнання агропромислового виробництва ', 'url' => ['/abiturient/napryam']],
+                            ['label' => 'Спеціальність 7.10010201  Процеси, машини та обладнання агропромислових підприємств', 'url' => ['/abiturient/spec7']],
+                            ['label' => 'Спеціальність 8.10010201  Процеси, машини та обладнання агропромислових підприємств', 'url' => ['/abiturient/spec8']],
+                            ['label' => 'Заочна форма навчання', 'url' => ['/abiturient/zaochna']],
+                        ],
+                    ],
+                    ['label' => 'Студенту',
+                        'items' => [
+                            ['label' => 'Предмети', 'url' => ['/predmet']],
+                            ['label' => 'Навчальні плани', 'url' => ['/student/plany']],
+                            ['label' => 'Робочі програми', 'url' => ['/student/teachers']],
+                            ['label' => 'Навчально-методичне забезпечення', 'url' => ['/metodychky']],
+                            ['label' => 'Дипломнику', 'url' => ['/student/dyplomnik']],
+                            ['label' => 'Корисні посилання', 'url' => ['/student/links']],
+                            ['label' => 'Програми практики', 'url' => ['/student/praktikprogramy']],
+                        ],
+                    ],
+                    ['label' => 'Наукова робота',
+                        'items' => [
+                            ['label' => 'Напрямки наукових досліджень викладачів кафедри', 'url' => ['/nauka/napryamy']],
+                            ['label' => 'Наукова робота студентів', 'url' => ['/nauka/student_work']],
+                            ['label' => 'Наукові заходи', 'url' => ['/nauka/zahody']],
+                            ['label' => 'Пропозиціії виробникам', 'url' => ['/nauka/proposal']],
+                            ['label' => 'Аспіранти кафедри', 'url' => ['/nauka/aspirant']],
+                        ],
+                    ],
                     Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+                        ['label' => 'Вхід на сайт', 'url' => ['/site/login']] :
+                        ['label' => 'Вийти (' . Yii::$app->user->identity->username . ')',
                             'url' => ['/site/logout'],
                             'linkOptions' => ['data-method' => 'post']],
                 ],
@@ -58,7 +98,7 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="pull-left">&copy; ПМОАПВ <?= date('Y') ?></p>
             <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
