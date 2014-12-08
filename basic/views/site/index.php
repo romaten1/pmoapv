3e182990e8d234aba4dce79ef208a86ebc45dd63
romaten1 <romaten1@rambler.ru>
2014-12-08 21:25:40 +0200

    Sly gallery

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -1,49 +1,83 @@
 <?php
 /* @var $this yii\web\View */
+use yii\helpers\Url;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>ВІТАЄМО!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Раді бачити вас на сайті <br />
+        <strong>кафедри процесів, машин та обладнання АПВ </strong><br />
+        Уманського національного університету садівництва.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <!-- <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
     </div>
+    
+    <?= philippfrenzel\yii2sly\yii2sly::widget([
+        'id' => 'sp_slider',
+        'items'=> [
+            ['content' => '<img src="' . Url::to('@web/img/sly/001.jpg') . '"></img>'],
+            ['content' => '<img src="' . Url::to('@web/img/sly/002.jpg') . '"></img>'],
+            ['content' => '<img src="' . Url::to('@web/img/sly/003.jpg') . '"></img>'],
+            ['content' => '<img src="' . Url::to('@web/img/sly/004.jpg') . '"></img>'],
+            ['content' => '<img src="' . Url::to('@web/img/sly/005.jpg') . '"></img>'],
+            ['content' => '<img src="' . Url::to('@web/img/sly/006.jpg') . '"></img>'],
+            ['content' => '<img src="' . Url::to('@web/img/sly/007.jpg') . '"></img>']
+        ],
+        'options' => [
+            'style' => "height:200px;",
+
+        ],
+        'clientOptions' => [
+            'horizontal' => 1,
+            'activateMiddle' => 1,
+            'itemNav' => 'centered',
+            'activateOn' =>   'click'
+                       
+        ]
+    ]); ?>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Історія кафедри</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Кафедра створена в 1929 році, коли Уманський агрополітехнікум був реорганізований в Уманський 
+                сільськогосподарський інститут. Першим завідувачем кафедри був професор В.Ф. Чалий. 
+                    Матеріальна база кафедри в перші роки складалася з машин і знарядь на кінній тязі. У 1935-1941 
+                    роках кафедра поповнилася тракторами, машинами на тракторній тязі, молотарками та ін. 
+                    Значний вклад у розвиток кафедри також зробили завідувачі Моторний К.П. (1944-1949 рр.), 
+                    В.Г. Красько (1956-1960 рр.), К.С. Хвиля (1960-1973 рр.), Сінгур М.К.(1973-1983 рр.),
+                    В.І. Марченко (1983-2009 рр.), Березовський А.П. (2009-2010 рр.), Войтік А.В. (2010-2012 рр.), 
+                    Вольвак С.Ф. (2012-2014 рр.).              
+                  </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href= <?= Url::to('@web/cafedra/page/history'); ?> >Далі ... &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Наукова робота</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Кафедра працює за комплексною науковою темою дослідження механізованих процесів агропромислового 
+                виробництва і має наукові розробки з питань підвищення рівня механізації в плодових розсадниках 
+                та ягідниках, паливної економічності та екологічності дизелів та ін. При кафедрі працює аспірантура, функціонує 
+                студентський науковий гурток, в якому студенти займаються науковою роботою.              </p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" href= <?= Url::to('@web/nauka/page/napryamy'); ?> >Далі ... &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Практична підготовка студентів</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Машинно-тракторний і автомобільний парк кафедри систематично поповнюється новими сільськогосподарськими 
+                машинами, обладнанням і автомобілями, що дозволяє проводити навчання студентів на базі сучасної 
+                сільськогосподарської та автомобільної техніки.
+                Студенти проходять навчальну практику на кафедральному полігоні і виробничу в 
+                умовах навчально-дослідного господарства та інших господарств регіону</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href= <?= Url::to('@web/cafedra/page/praktika'); ?> >Далі ... &raquo;</a></p>
             </div>
         </div>
 
