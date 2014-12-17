4b4978e4f8769be643a7ad6195958b726d91e07f
romaten1 <romaten1@rambler.ru>
2014-12-17 18:41:10 +0200

    Little change in predmet and teacher view

--- basic/modules/admin/views/predmet/viewAdmin.php
+++ basic/modules/admin/views/predmet/viewAdmin.php
@@ -31,7 +31,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'title',
             [
                 'attribute' => 'description',
--- basic/modules/admin/views/teacher/viewAdmin.php
+++ basic/modules/admin/views/teacher/viewAdmin.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use kartik\icons\Icon;
+use app\models\TeachPredmet;
+use app\models\Predmet;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Teacher */
@@ -43,5 +45,20 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ],
     ]) ?>
+    Предмети, які веде викладач:<br />
+        <?
+            $predmet_id = TeachPredmet::findAll([
+                'teach_id' => $model->id,
+            ]); 
+            foreach($predmet_id as $predmet){
+                $predmet_name = Predmet::findOne($predmet->predmet_id);
+                echo 
+                Html::a($predmet_name->title, 
+                    ['/admin/predmet/view', 'id' => $predmet_name->id], ['class' => 'btn btn-default'])
+                . '<br />'; 
+            }
+            
+
+        ?>
 
 </div>
