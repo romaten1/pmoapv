a24fb23814f89f7b5b28a5f12aa2da176f604d93
romaten1 <romaten1@rambler.ru>
2015-04-08 20:32:36 +0300

    Sensio Labs Insight fixed errors

--- basic/helpers/FileHelper.php
+++ basic/helpers/FileHelper.php
@@ -2,9 +2,6 @@
 namespace app\helpers;
 
 use Yii;
-use yii\web\UploadedFile;
-use yii\imagine\Image;
-use app\helpers\TransliterateHelper;
 /**
  * Данный класс позволяет переводить строку с крилицы на латынь.
  */
@@ -28,41 +25,5 @@ class FileHelper
 		    } else { 
 		        return round($size / $tb, 2).' Тб'; 
 		    } 
-		}	
-		
-        public static function createImage($model, $module, $attribute = 'image_id' )
-		{
-		// Получаем массив данных по загружамых файлах
-            $image_id = UploadedFile::getInstance($model, $attribute);
-            $filename = $image_id->name;
-            $ext = end((explode(".", $image_id->name)));
-            // Генерируем уникальное имя файла
-            $model->title_en =  TransliterateHelper::cyrillicToLatin($model->title);
-            $model->image_id = $attribute . '-'. 
-            					substr($model->title_en, 0, 6) . '-' . 
-            					//Yii::$app->getSecurity()->generateRandomString(5). '.'.$ext;
-            					'abcde.'.$ext;
-            $image_name = $model->image_id;
-            // Путь к папке где будет хранится файл
-            $path = \Yii::$app->basePath . '/uploads/'.$module.'/' . $model->image_id;
-            
-            if($model->save()){
-                
-                // Сохраняем рисунок
-                $image_id->saveAs($path);
-
-                $image_full = \Yii::$app->image->load($path);
-                $full_path = \Yii::$app->basePath . '/uploads/'.$module.'/' . $image_name;
-                $image_full->resize('600', '400');
-                $image_full->save($full_path);
-                // Обработка загруженного изображения и создание thumbs
-                $image_less = \Yii::$app->image->load($path);
-                $less_path = \Yii::$app->basePath . '/uploads/'.$module.'/' .' thumbs/thumb_' . $image_name;
-                $image_less->resize('50', '50');
-                $image_less->save($less_path);
-				return $this->redirect(['view', 'id'=>$model->id]);
-            } else {
-                throw new NotFoundHttpException('Не удалось загрузить данные');
-            }  
-        }   
+		}
 }
